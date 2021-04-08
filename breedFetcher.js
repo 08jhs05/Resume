5486f6e103f9595de7a724382e8585aa2201851a
JayShin <08jhs05@gmail.com>
2021-04-08 16:31:28 -0400

    add test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,16 @@
 const request = require('request');
 
-const catQuery = process.argv[2];
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + catQuery;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-request(url, (error, response, body) => {
-  if (error) throw new Error('Request failed!!!');
-  const data = JSON.parse(body);
-  if (data[0] && data[0].description) console.log(data[0].description);
-  else console.log(`Breed '${catQuery}' not found!`);
-});
+  request(url, (error, response, body) => {
+    if (error) throw new Error('Request failed!!!');
+    const data = JSON.parse(body);
+    let desc = '';
+    if (data[0] && data[0].description) desc = data[0].description;
+    else desc = `Breed '${breedName}' not found!`;
+    callback(error, desc);
+  });
+};
+
+module.exports = { fetchBreedDescription };
